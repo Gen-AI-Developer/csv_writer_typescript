00f8647e9b5fb7f7beb2eeb0fe76f9467052200d
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-05 01:04:36 +0500

    compelted

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,30 @@ interface Payment {
     to: string,
     notes: string
 }
+type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[]
 class CSVWriter {
-    constructor() { }
-
+    constructor(private columns: PaymentColumns) {
+        this.csv = this.columns.join(',')
+    }
+    private csv: string
+    addRows(values: Payment[]): void {
+        let rows = values.map((v) => this.formatRow(v))
+        this.csv += rows.join('\n');
+        console.log(this.csv)
+    }
+    private formatRow(p: Payment): string {
+        return this.columns.map((col) => p[col]).join(',')
+    }
 }
+
+const writer = new CSVWriter(['id', 'amount', 'to', 'notes'])
+writer.addRows(
+    [
+        {
+            id: 1,
+            amount: 1234,
+            to: 'safdar',
+            notes: 'this is a note'
+        }
+    ]
+)
